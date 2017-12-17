08498783f20c64ba0e55cee0f00149dcb135cd74
Paul Beaudet <inof8or@gmail.com>
2017-12-17 15:11:47 -0500

    Provide a better ecosystem example

--- sampleEcosystem.config.js
+++ sampleEcosystem.config.js
@@ -8,9 +8,19 @@ module.exports = {                                                          // R
     script : "./pushdetector.js",                                           // relitive executable assuming cofiguration is in same folder as service
     watch  : true,                                                          // service will restart when script file is changed
     env    : {
-      "MAIN_MONGO": "address to database with appointments for webservice", // one can use mLab or MongoAtlas
-      "MONGODB_URI": "address to database for push status",                 // this database could be run locally as well as above options
-      "KEY": "key to decrypt configuration for push notifications"
-    }
-  }]
+            "MAIN_MONGO": "address to database with appointments for webservice", // one can use mLab or MongoAtlas
+            "MONGODB_URI": "address to database for push status",                 // this database could be run locally as well as above options
+            "KEY": "key to decrypt configuration for push notifications"
+        },
+    },{                               // optional continuous deployment process that responds to changes in github "npm install -g jitploy"
+        name   : "cdForPushdetector", // make sure name has different first characters
+        script : "./cd.sh",           // create an executable script that uses jitploy
+        watch  : false,               // This will do weird things if set to true, its pointing at same directory
+    }]
 };
+
+/*  example of cd.sh
+ #/bin/bash
+ # npm install -g jitploy
+ jitploy pushdetector.js -pm2 true -repo "urfork/pushdetector" --token "urtoken" --server "urserver"
+ */
